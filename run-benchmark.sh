90b9c78bcf12b5c0430a5e02f009ed7f6aee6806
Johannes Thumshirn <jthumshirn@suse.de>
2016-07-06 15:35:07 +0200

    Add information about the target to the benchmark info
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -56,11 +56,23 @@ full=$OUT/full
 truncate --size 0 $terse
 truncate --size 0 $full
 
+if [ -b $FILE ]; then
+	devname=$FILE
+else
+	devname=$(df $FILE | cut -d ' ' -f 1)
+fi
+vendor=$(sg_inq $devname | grep "Vendor identification:" | \
+	cut -d ':' -f 2 | sed 's/ //')
+model=$(sg_inq $devname | grep " Product identification:" | \
+	cut -d ':' -f 2 | sed 's/ //')
+
 for bench in $BENCHES; do
 	out="$bench-$(echo $FILE | tr '/' '-' | tr ' ' '-')"
 	echo "$bench:" | tee -a $terse
 	echo "$bench:" | tee -a $full
 	$BENCHES/$bench $FILE > $out
+
+	echo "**** Device $devname ($vendor $model) benchmark $bench:" >> $full
 	cat $out >> $full
 	echo >> $full
 
